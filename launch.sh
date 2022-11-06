a4a68eae7848126f305b509afc4d72193cc21b65
pemukl <account@shnei.de>
2022-11-05 22:16:15 -0700

    Incremented sleeping time

--- launch.sh
+++ launch.sh
@@ -1,10 +1,10 @@
 source config.sh
 aws ec2 run-instances --launch-template LaunchTemplateId=$TEMPLATE_ID --count=5  > /dev/null
 
-echo "Started instances. Waiting 40sec for them to be ready."
+echo "Started instances. Waiting 60sec for them to be ready."
+sleep 30
+echo "30sec"
 sleep 20
-echo "20sec"
-sleep 10
 echo "10sec"
 sleep 10
 echo "Starting setup."
